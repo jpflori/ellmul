08894bd21d967ece69c6e98d5aeb96bc40f2f94e
Jean-Pierre Flori <jean-pierre.flori@ssi.gouv.fr>
2014-12-23 17:14:34 +0100

    Correct software name in license text.

--- fq_weierstrass_xz/clear.c
+++ fq_weierstrass_xz/clear.c
@@ -1,19 +1,19 @@
 /*=============================================================================
 
-    This file is part of FLINT.
+    This file is part ofELLMUL
 
-    FLINT is free software; you can redistribute it and/or modify
+   ELLMULis free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 3 of the License, or
     (at your option) any later version.
 
-    FLINT is distributed in the hope that it will be useful,
+   ELLMULis distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with FLINT; if not, write to the Free Software
+    along withELLMUL if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 =============================================================================*/
